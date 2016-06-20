c7f7661927a3a0659274745fe6dae524d3745211
dbdkmezz <dbdkmezz@gmail.com>
2016-06-20 15:42:36 +0100

    added title

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ var SampleApp = function() {
 
         self.routes['/'] = function(req, res) {
             res.setHeader('Content-Type', 'text/html');
-            res.send("<p><h2>Hello lovely Verity!</h2> <p><img src='static/asbw.jpg'>");
+            res.send("<html><head><title>Verity and Paul's Wedding Website</title></head><body><p><h2>Hello lovely Verity!</h2> <p><img src='static/asbw.jpg'></body></html>");
         };
 	
 	// self.routes['/asbw.jpg'] = function(req, res) {
