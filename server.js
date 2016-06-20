7be1d39699f12a504f8a1be0c86a74a43c21e81a
dbdkmezz <dbdkmezz@gmail.com>
2016-06-20 14:21:38 +0100

    verity

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ var SampleApp = function() {
 
         self.routes['/'] = function(req, res) {
             res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('index.html') );
+            res.send('Hello Verity'); //self.cache_get('index.html') );
         };
     };
 
